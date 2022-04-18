61c3986a1905b7b02fa7ecfbec73290f51c1ea2b
Matheus Raposo <matheus_raposo@hotmail.com>
2022-04-18 11:47:57 -0300

    add route to portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Outlet, Link} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello, <br />
-          I'm Matheus Raposo, <br />
-          Full Stack Developer
-        </p>
-        <button
-          onClick={()=>{console.log('1')}}
-          style={{textDecorationLine: "underline",
-          color: 'blue'}}
-        >
-          Portfolio
-        </button>
-      </header>
-    </div>
-  );
+    return (
+            <div className="App">
+                <header className="App-header">
+                    <nav></nav>
+                    <p>
+                        Hello, <br/>
+                        I'm Matheus Raposo, <br/>
+                        Full Stack Developer
+                    </p>
+                    <Link to="/portfolio">Portfolio</Link>
+                </header>
+                <Outlet/>
+            </div>
+    );
 }
 
 export default App;
---
+++ src/routes/portfolio.tsx
@@ -0,0 +1,11 @@
+const Portfolio = () => {
+    return (
+            <main style={{padding: "1rem 0"}}>
+                <ul>
+                    <li>Proj 1</li>
+                </ul>
+            </main>
+    )
+}
+
+export default Portfolio;
