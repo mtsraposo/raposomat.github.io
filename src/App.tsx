3294dc96dc098f8ccca735eac326e9baf50721ca
Matheus Raposo <matheus_raposo@hotmail.com>
2022-04-18 11:13:35 -0300

    add front page

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Hello, <br />
+          I'm Matheus Raposo, <br />
+          Full Stack Developer
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <button
+          onClick={()=>{console.log('1')}}
+          style={{textDecorationLine: "underline",
+          color: 'blue'}}
         >
-          Learn React
-        </a>
+          Portfolio
+        </button>
       </header>
     </div>
   );
